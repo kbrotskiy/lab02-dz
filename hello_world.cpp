20deb1ac3dc77d016651784f98acaf7e3c903d95
kbrotskiy <brotskia_26@mail.ru>
2020-04-06 21:13:29 +0300

    without namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main(int argc, const char * argv[]) {
     string name;
-    cin>>name;
-    cout << "Hello World from"<<name;
+    std::cin>>name;
+    std::cout << "Hello World from"<<name;
     return 0;
 }
